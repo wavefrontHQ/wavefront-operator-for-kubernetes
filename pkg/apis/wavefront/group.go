02b89af9bd01280d56873a079083dc91fb59a548
John Cornish <jcornish@vmware.com>
2022-03-31 11:04:29 -0600

    WIP: start fresh
    Signed-off-by: Helen Shao <shaoh@vmware.com>

--- pkg/apis/wavefront/group.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 VMware, Inc. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// Package wavefront contains wavefront API versions.
-//
-// This file ensures Go source parsers acknowledge the wavefront package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package wavefront
--- pkg/apis/wavefront/v1alpha1/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2020 VMware, Inc. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// Package v1alpha1 contains API Schema definitions for the wavefront v1alpha1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=wavefront.com
-package v1alpha1
