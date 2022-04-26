66887d649b43a713daa9b4e8ef0d2b2236171a49
Mark Michael <mamichael@vmware.com>
2022-04-26 12:30:43 -0600

    WIP: broken test

--- controllers/wavefront_controller_test.go
+++ controllers/wavefront_controller_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	wavefrontcomv1alpha1 "github.com/wavefrontHQ/wavefront-operator-for-kubernetes/api/v1alpha1"
 	"github.com/wavefrontHQ/wavefront-operator-for-kubernetes/controllers"
-	v1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -18,17 +17,85 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	testing2 "k8s.io/client-go/testing"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 func TestReconcile(t *testing.T) {
+
+	//t.Run("creates proxy and service", func(t *testing.T) {
+	//	 client, dynamicClient := setupCreate("testUrl", "testToken")
+	//
+	//	r := &controllers.WavefrontReconciler{
+	//		Client:        client,
+	//		Scheme:        nil,
+	//		FS:            os.DirFS("../deploy"),
+	//		DynamicClient: dynamicClient,
+	//		RestMapper:    client.RESTMapper(),
+	//	}
+	//	results, err := r.Reconcile(context.Background(), reconcile.Request{})
+	//
+	//	assert.NoError(t, err)
+	//	assert.Equal(t, ctrl.Result{}, results)
+	//	assert.Equal(t, 4, len(dynamicClient.Actions()))
+	//	assert.Equal(t, "services", dynamicClient.Actions()[1].GetResource().Resource)
+	//	assert.Equal(t, "deployments", dynamicClient.Actions()[3].GetResource().Resource)
+	//
+	//	deploymentObject := dynamicClient.Actions()[3].(testing2.CreateActionImpl).GetObject().(*unstructured.Unstructured)
+	//	var deployment v1.Deployment
+	//	err = runtime.DefaultUnstructuredConverter.FromUnstructured(deploymentObject.Object, &deployment)
+	//
+	//	assert.NoError(t, err)
+	//	assert.Equal(t, "testUrl/api/", deployment.Spec.Template.Spec.Containers[0].Env[0].Value)
+	//	assert.Equal(t, "testToken", deployment.Spec.Template.Spec.Containers[0].Env[1].Value)
+	//})
+
+	t.Run("updates proxy and service", func(t *testing.T) {
+		client, dynamicClient := setupPatch("testUrl", "updatedToken")
+
+		r := &controllers.WavefrontReconciler{
+			Client:        client,
+			Scheme:        nil,
+			FS:            os.DirFS("../deploy"),
+			DynamicClient: dynamicClient,
+			RestMapper:    client.RESTMapper(),
+		}
+		results, err := r.Reconcile(context.Background(), reconcile.Request{})
+
+		assert.NoError(t, err)
+		assert.Equal(t, ctrl.Result{}, results)
+		assert.Equal(t, 2, len(dynamicClient.Actions()))
+		//assert.Equal(t, "services", dynamicClient.Actions()[0].GetResource().Resource)
+		//assert.Equal(t, "deployments", dynamicClient.Actions()[1].GetResource().Resource)
+
+		deploymentObject := dynamicClient.Actions()[3].(testing2.PatchActionImpl).Patch
+		//var deployment v1.Deployment
+		//err = runtime.DefaultUnstructuredConverter.FromUnstructured(deploymentObject.Object, &deployment)
+		assert.Contains(t,  string(deploymentObject), "updatedToken")
+
+		assert.NoError(t, err)
+		//assert.Equal(t, "testUrl/api/", deployment.Spec.Template.Spec.Containers[0].Env[0].Value)
+		//assert.Equal(t, "updatedToken", deployment.Spec.Template.Spec.Containers[0].Env[1].Value)
+	})
+}
+
+func setupCreate(wavefrontUrl, wavefrontToken string) (client.WithWatch, *dynamicfake.FakeDynamicClient) {
+	return setup(wavefrontUrl, wavefrontToken, "testProxy", "testNamespace")
+}
+
+func setupPatch(wavefrontUrl, wavefrontToken string) (client.WithWatch, *dynamicfake.FakeDynamicClient) {
+	return setup(wavefrontUrl, wavefrontToken, "wavefront-proxy", "wavefront")
+}
+
+func setup(wavefrontUrl, wavefrontToken, wavefrontProxyName, namespace string) (client.WithWatch, *dynamicfake.FakeDynamicClient) {
 	wf := &wavefrontcomv1alpha1.Wavefront{
 		TypeMeta:   metav1.TypeMeta{},
 		ObjectMeta: metav1.ObjectMeta{},
-		Spec:       wavefrontcomv1alpha1.WavefrontSpec{WavefrontUrl: "testUrl", WavefrontToken: "testToken"},
+		Spec:       wavefrontcomv1alpha1.WavefrontSpec{WavefrontUrl: wavefrontUrl, WavefrontToken: wavefrontToken},
 		Status:     wavefrontcomv1alpha1.WavefrontStatus{},
 	}
+
 	s := scheme.Scheme
 	s.AddKnownTypes(wavefrontcomv1alpha1.GroupVersion, wf)
 
@@ -58,8 +125,8 @@ func TestReconcile(t *testing.T) {
 				"apiVersion": "apps/v1",
 				"kind":       "Deployment",
 				"metadata": map[string]interface{}{
-					"name":      "testProxy",
-					"namespace": "testNamespace",
+					"name":      wavefrontProxyName,
+					"namespace": namespace,
 				},
 				"spec": map[string]interface{}{
 					"testSpec": "3",
@@ -71,8 +138,8 @@ func TestReconcile(t *testing.T) {
 				"apiVersion": "v1",
 				"kind":       "Service",
 				"metadata": map[string]interface{}{
-					"name":      "testName",
-					"namespace": "testNamespace",
+					"name":      wavefrontProxyName,
+					"namespace": namespace,
 				},
 				"spec": map[string]interface{}{
 					"testSpec": "3",
@@ -80,64 +147,5 @@ func TestReconcile(t *testing.T) {
 			},
 		},
 	)
-
-	t.Run("creates proxy and service", func(t *testing.T) {
-		r := &controllers.WavefrontReconciler{
-			Client:        client,
-			Scheme:        nil,
-			FS:            os.DirFS("../deploy"),
-			DynamicClient: dynamicClient,
-			RestMapper:    client.RESTMapper(),
-		}
-		results, err := r.Reconcile(context.Background(), reconcile.Request{})
-
-		assert.NoError(t, err)
-		assert.Equal(t, ctrl.Result{}, results)
-		assert.Equal(t, 4, len(dynamicClient.Actions()))
-		assert.Equal(t, "services", dynamicClient.Actions()[1].GetResource().Resource)
-		assert.Equal(t, "deployments", dynamicClient.Actions()[3].GetResource().Resource)
-
-		deploymentObject := dynamicClient.Actions()[3].(testing2.CreateActionImpl).GetObject().(*unstructured.Unstructured)
-		var deployment v1.Deployment
-		err = runtime.DefaultUnstructuredConverter.FromUnstructured(deploymentObject.Object, &deployment)
-
-		assert.NoError(t, err)
-		assert.Equal(t, "testUrl/api/", deployment.Spec.Template.Spec.Containers[0].Env[0].Value)
-		assert.Equal(t, "testToken", deployment.Spec.Template.Spec.Containers[0].Env[1].Value)
-	})
-	t.Run("updates proxy and service", func(t *testing.T) {
-		wfUpdated := &wavefrontcomv1alpha1.Wavefront{
-			TypeMeta:   metav1.TypeMeta{},
-			ObjectMeta: metav1.ObjectMeta{},
-			Spec:       wavefrontcomv1alpha1.WavefrontSpec{WavefrontUrl: "testUrl", WavefrontToken: "updatedToken"},
-			Status:     wavefrontcomv1alpha1.WavefrontStatus{},
-		}
-
-		clientBuilder := fake.NewClientBuilder()
-		clientBuilder = clientBuilder.WithScheme(s).WithObjects(wfUpdated).WithRESTMapper(testRestMapper)
-		client = clientBuilder.Build()
-
-		r := &controllers.WavefrontReconciler{
-			Client:        client,
-			Scheme:        nil,
-			FS:            os.DirFS("../deploy"),
-			DynamicClient: dynamicClient,
-			RestMapper:    client.RESTMapper(),
-		}
-		results, err := r.Reconcile(context.Background(), reconcile.Request{})
-
-		assert.NoError(t, err)
-		assert.Equal(t, ctrl.Result{}, results)
-		assert.Equal(t, 4, len(dynamicClient.Actions()))
-		assert.Equal(t, "services", dynamicClient.Actions()[1].GetResource().Resource)
-		assert.Equal(t, "deployments", dynamicClient.Actions()[3].GetResource().Resource)
-
-		deploymentObject := dynamicClient.Actions()[3].(testing2.CreateActionImpl).GetObject().(*unstructured.Unstructured)
-		var deployment v1.Deployment
-		err = runtime.DefaultUnstructuredConverter.FromUnstructured(deploymentObject.Object, &deployment)
-
-		assert.NoError(t, err)
-		assert.Equal(t, "testUrl/api/", deployment.Spec.Template.Spec.Containers[0].Env[0].Value)
-		assert.Equal(t, "updatedToken", deployment.Spec.Template.Spec.Containers[0].Env[1].Value)
-	})
+	return client, dynamicClient
 }
