da4f5e9d3e686b1fde1c74e5e1d9918d203e0949
Mark Michael <mamichael@vmware.com>
2022-05-02 14:24:44 -0600

    WIP: namespace and role
    Signed-off-by: Helen Shao <shaoh@vmware.com>

--- controllers/wavefront_controller.go
+++ controllers/wavefront_controller.go
@@ -64,6 +64,7 @@ type WavefrontReconciler struct {
 // +kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps,resources=daemonsets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
