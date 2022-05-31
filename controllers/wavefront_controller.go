793e65af38d6e367c7174a5afda5d20c2f724033
Matthew Conger-Eldeen <matthewco@vmware.com>
2022-05-31 14:51:44 -0600

    remove list permissions from many resources
    Signed-off-by: John Cornish <jcornish@vmware.com>

--- controllers/wavefront_controller.go
+++ controllers/wavefront_controller.go
@@ -66,18 +66,14 @@ type WavefrontReconciler struct {
 // +kubebuilder:rbac:groups=wavefront.com,resources=wavefronts,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=wavefront.com,resources=wavefronts/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=wavefront.com,resources=wavefronts/finalizers,verbs=update
-// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;create;update;patch;delete
-// +kubebuilder:rbac:groups="",resources=services,verbs=get;list;create;update;patch;delete
-// +kubebuilder:rbac:groups=apps,resources=daemonsets,verbs=get;list;create;update;patch;delete
-// +kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=get;list;create;update;patch;delete
-// +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=services,verbs=get;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps,resources=daemonsets,verbs=get;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=get;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Wavefront object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
