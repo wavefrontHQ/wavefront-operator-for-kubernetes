4fd36db029dbc3c977b09cec6d9041f08b4ab51e
Helen Shao <43485165+helen-shao@users.noreply.github.com>
2022-07-19 10:05:21 -0600

    K8SSAAS-998 cross validation (#107)
    Signed-off-by: Anil Kodali <akodali@vmware.com>

--- internal/health/status_test.go
+++ internal/health/status_test.go
@@ -57,7 +57,7 @@ func TestReconcileReportHealthStatus(t *testing.T) {
 			"wavefront-node-collector": {},
 		}
 
-		healthy, message := UpdateComponentStatuses(appsV1, deploymentStatuses, daemonSetStatuses)
+		warnings, healthy, message := UpdateComponentStatuses(appsV1, deploymentStatuses, daemonSetStatuses, &wf.Wavefront{})
 		assert.True(t, healthy)
 		assert.Equal(t, "(3/3) wavefront components are healthy.", message)
 		assert.True(t, deploymentStatuses["wavefront-proxy"].Healthy)
@@ -66,6 +66,7 @@ func TestReconcileReportHealthStatus(t *testing.T) {
 		assert.Equal(t, "Running (1/1)", deploymentStatuses["wavefront-proxy"].Status)
 		assert.True(t, daemonSetStatuses["wavefront-node-collector"].Healthy)
 		assert.Equal(t, "Running (3/3)", daemonSetStatuses["wavefront-node-collector"].Status)
+		assert.False(t, warnings)
 	})
 
 	t.Run("report health status when one component is unhealthy", func(t *testing.T) {
@@ -112,7 +113,8 @@ func TestReconcileReportHealthStatus(t *testing.T) {
 			"wavefront-node-collector": {},
 		}
 
-		healthy, message := UpdateComponentStatuses(appsV1, deploymentStatuses, daemonSetStatuses)
+		warnings, healthy, message := UpdateComponentStatuses(appsV1, deploymentStatuses, daemonSetStatuses, &wf.Wavefront{})
+		assert.False(t, warnings)
 		assert.False(t, healthy)
 		assert.Equal(t, "(2/3) wavefront components are healthy.", message)
 	})
@@ -149,7 +151,8 @@ func TestReconcileReportHealthStatus(t *testing.T) {
 			"wavefront-node-collector": {},
 		}
 
-		healthy, message := UpdateComponentStatuses(appsV1, deploymentStatuses, daemonSetStatuses)
+		warnings, healthy, message := UpdateComponentStatuses(appsV1, deploymentStatuses, daemonSetStatuses, &wf.Wavefront{})
+		assert.False(t, warnings)
 		assert.True(t, healthy)
 		assert.Equal(t, "(2/2) wavefront components are healthy.", message)
 	})
@@ -207,7 +210,8 @@ func TestReconcileReportHealthStatus(t *testing.T) {
 			},
 		}
 
-		healthy, message := UpdateComponentStatuses(appsV1, deploymentStatuses, daemonSetStatuses)
+		warnings, healthy, message := UpdateComponentStatuses(appsV1, deploymentStatuses, daemonSetStatuses, &wf.Wavefront{})
+		assert.False(t, warnings)
 		assert.True(t, healthy)
 		assert.Equal(t, "(3/3) wavefront components are healthy.", message)
 		assert.True(t, deploymentStatuses["wavefront-proxy"].Healthy)
@@ -216,6 +220,71 @@ func TestReconcileReportHealthStatus(t *testing.T) {
 		assert.Equal(t, "Running (3/3)", daemonSetStatuses["wavefront-node-collector"].Status)
 	})
 
+	t.Run("clear out previous values when updating status", func(t *testing.T) {
+		proxyDeployment := &appsv1.Deployment{
+			TypeMeta: metav1.TypeMeta{},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "wavefront-proxy",
+				Namespace: "wavefront",
+			},
+			Status: appsv1.DeploymentStatus{
+				Replicas:          1,
+				AvailableReplicas: 1,
+			},
+		}
+		collectorDeployment := &appsv1.Deployment{
+			TypeMeta: metav1.TypeMeta{},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "wavefront-cluster-collector",
+				Namespace: "wavefront",
+			},
+			Status: appsv1.DeploymentStatus{
+				Replicas:          1,
+				AvailableReplicas: 1,
+			},
+		}
+		collectorDaemonSet := &appsv1.DaemonSet{
+			TypeMeta: metav1.TypeMeta{},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "wavefront-node-collector",
+				Namespace: "wavefront",
+			},
+			Status: appsv1.DaemonSetStatus{
+				DesiredNumberScheduled: 3,
+				NumberReady:            3,
+			},
+		}
+
+		appsV1 := setup(proxyDeployment, collectorDeployment, collectorDaemonSet)
+		deploymentStatuses := map[string]*wf.DeploymentStatus{
+			"wavefront-proxy": &wf.DeploymentStatus{
+				Message: "previous proxy message",
+				Status:  "Running (0/1)",
+				Healthy: false,
+			},
+
+			"wavefront-cluster-collector": {},
+		}
+		daemonSetStatuses := map[string]*wf.DaemonSetStatus{
+			"wavefront-node-collector": &wf.DaemonSetStatus{
+				Message: "previous collector message",
+				Status:  "Running (0/1)",
+				Healthy: false,
+			},
+		}
+
+		warnings, healthy, message := UpdateComponentStatuses(appsV1, deploymentStatuses, daemonSetStatuses, &wf.Wavefront{
+			Spec: wf.WavefrontSpec{
+				DataExport: wf.DataExport{
+					ExternalWavefrontProxy: wf.ExternalWavefrontProxy{Url: "testURL"},
+					WavefrontProxy:         wf.WavefrontProxy{Enable: true},
+				},
+			},
+		})
+		assert.True(t, healthy)
+		assert.Contains(t, message, "Warning")
+		assert.True(t, warnings)
+	})
 }
 
 func setup(initObjs ...runtime.Object) typedappsv1.AppsV1Interface {
