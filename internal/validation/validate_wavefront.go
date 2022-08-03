33095aa49339c028dd35d536b82c0c36a73f50b4
Mark Michael <mamichael@vmware.com>
2022-08-03 11:19:45 -0600

    Adds resource checks for node and cluster collector and empheral-storage
    Signed-off-by: Priya Selvaganesan <pselvaganesa@vmware.com>

--- internal/validation/validate_wavefront.go
+++ internal/validation/validate_wavefront.go
@@ -16,6 +16,10 @@ func Validate(wavefront *wf.Wavefront) error {
 		}
 		errs = append(errs, validateResources(&wavefront.Spec.DataExport.WavefrontProxy.Resources, "spec.dataExport.wavefrontProxy")...)
 	}
+	if wavefront.Spec.DataCollection.Metrics.Enable {
+		errs = append(errs, validateResources(&wavefront.Spec.DataCollection.Metrics.NodeCollector.Resources, "spec.dataCollection.metrics.nodeCollector")...)
+		errs = append(errs, validateResources(&wavefront.Spec.DataCollection.Metrics.ClusterCollector.Resources, "spec.dataCollection.metrics.clusterCollector")...)
+	}
 	return utilerrors.NewAggregate(errs)
 }
 
@@ -23,10 +27,13 @@ func validateResources(resources *wf.Resources, resourcePath string) []error {
 	var errs []error
 
 	if compareQuantities(resources.Requests.CPU, resources.Limits.CPU) > 0 {
-		errs = append(errs, fmt.Errorf("invalid %s.resources.requests.cpu: %s must be less than or equal to cpu limit.", resourcePath, resources.Requests.CPU))
+		errs = append(errs, fmt.Errorf("invalid %s.resources.requests.cpu: %s must be less than or equal to cpu limit", resourcePath, resources.Requests.CPU))
 	}
 	if compareQuantities(resources.Requests.Memory, resources.Limits.Memory) > 0 {
-		errs = append(errs, fmt.Errorf("invalid %s.resources.requests.memory: %s must be less than or equal to memory limit.", resourcePath, resources.Requests.Memory))
+		errs = append(errs, fmt.Errorf("invalid %s.resources.requests.memory: %s must be less than or equal to memory limit", resourcePath, resources.Requests.Memory))
+	}
+	if compareQuantities(resources.Requests.EphemeralStorage, resources.Limits.EphemeralStorage) > 0 {
+		errs = append(errs, fmt.Errorf("invalid %s.resources.requests.ephemeral-storage: %s must be less than or equal to ephemeral-storage limit", resourcePath, resources.Requests.EphemeralStorage))
 	}
 	return errs
 }
--- internal/validation/validate_wavefront_test.go
+++ internal/validation/validate_wavefront_test.go
@@ -20,11 +20,11 @@ func TestValidate(t *testing.T) {
 		assert.Equal(t, "'externalWavefrontProxy.url' and 'wavefrontProxy.enable' should not be set at the same time", Validate(wfCR).Error())
 	})
 
-	t.Run("Validation error when proxy CPU request is greater than CPU limit", func(t *testing.T) {
+	t.Run("Validation error when CPU request is greater than CPU limit", func(t *testing.T) {
 		wfCR := defaultWFCR()
 		wfCR.Spec.DataExport.WavefrontProxy.Resources.Requests.CPU = "500m"
 		wfCR.Spec.DataExport.WavefrontProxy.Resources.Limits.CPU = "200m"
-		assert.Equal(t, "invalid spec.dataExport.wavefrontProxy.resources.requests.cpu: 500m must be less than or equal to cpu limit.", Validate(wfCR).Error())
+		assert.Equal(t, "invalid spec.dataExport.wavefrontProxy.resources.requests.cpu: 500m must be less than or equal to cpu limit", Validate(wfCR).Error())
 	})
 
 	t.Run("CPU expressed differently should not be an error", func(t *testing.T) {
@@ -34,13 +34,49 @@ func TestValidate(t *testing.T) {
 		assert.Nilf(t, Validate(wfCR), "did not expect validation error")
 	})
 
-	t.Run("Validation error when proxy Memory request is greater than Memory limit", func(t *testing.T) {
+	t.Run("Validation error when Memory request is greater than Memory limit", func(t *testing.T) {
 		wfCR := defaultWFCR()
 		wfCR.Spec.DataExport.WavefrontProxy.Resources.Requests.Memory = "500Mi"
 		wfCR.Spec.DataExport.WavefrontProxy.Resources.Limits.Memory = "200Mi"
 		validationError := Validate(wfCR)
 		assert.NotNilf(t, validationError, "expected validation error")
-		assert.Equal(t, "invalid spec.dataExport.wavefrontProxy.resources.requests.memory: 500Mi must be less than or equal to memory limit.", validationError.Error())
+		assert.Equal(t, "invalid spec.dataExport.wavefrontProxy.resources.requests.memory: 500Mi must be less than or equal to memory limit", validationError.Error())
+	})
+
+	t.Run("Validation error when EphemeralStorage request is greater than limit", func(t *testing.T) {
+		wfCR := defaultWFCR()
+		wfCR.Spec.DataExport.WavefrontProxy.Resources.Requests.EphemeralStorage = "1Gi"
+		wfCR.Spec.DataExport.WavefrontProxy.Resources.Limits.EphemeralStorage = "500Mi"
+		validationError := Validate(wfCR)
+		assert.NotNilf(t, validationError, "expected validation error")
+		assert.Equal(t, "invalid spec.dataExport.wavefrontProxy.resources.requests.ephemeral-storage: 1Gi must be less than or equal to ephemeral-storage limit", validationError.Error())
+	})
+
+	t.Run("Validation error om node collector resources", func(t *testing.T) {
+		wfCR := defaultWFCR()
+		wfCR.Spec.DataCollection.Metrics.NodeCollector.Resources.Requests.CPU = "500m"
+		wfCR.Spec.DataCollection.Metrics.NodeCollector.Resources.Limits.CPU = "200m"
+		assert.Equal(t, "invalid spec.dataCollection.metrics.nodeCollector.resources.requests.cpu: 500m must be less than or equal to cpu limit", Validate(wfCR).Error())
+	})
+
+	t.Run("Validation error on cluster resources", func(t *testing.T) {
+		wfCR := defaultWFCR()
+		wfCR.Spec.DataCollection.Metrics.ClusterCollector.Resources.Requests.Memory = "500Mi"
+		wfCR.Spec.DataCollection.Metrics.ClusterCollector.Resources.Limits.Memory = "200Mi"
+		validationError := Validate(wfCR)
+		assert.NotNilf(t, validationError, "expected validation error")
+		assert.Equal(t, "invalid spec.dataCollection.metrics.clusterCollector.resources.requests.memory: 500Mi must be less than or equal to memory limit", validationError.Error())
+	})
+
+	t.Run("Test multiple errors", func(t *testing.T) {
+		wfCR := defaultWFCR()
+		wfCR.Spec.DataCollection.Metrics.ClusterCollector.Resources.Requests.Memory = "500Mi"
+		wfCR.Spec.DataCollection.Metrics.ClusterCollector.Resources.Limits.Memory = "200Mi"
+		wfCR.Spec.DataCollection.Metrics.ClusterCollector.Resources.Requests.CPU = "500m"
+		wfCR.Spec.DataCollection.Metrics.ClusterCollector.Resources.Limits.CPU = "200m"
+		validationError := Validate(wfCR)
+		assert.NotNilf(t, validationError, "expected validation error")
+		assert.Equal(t, "[invalid spec.dataCollection.metrics.clusterCollector.resources.requests.cpu: 500m must be less than or equal to cpu limit, invalid spec.dataCollection.metrics.clusterCollector.resources.requests.memory: 500Mi must be less than or equal to memory limit]", validationError.Error())
 	})
 }
 
