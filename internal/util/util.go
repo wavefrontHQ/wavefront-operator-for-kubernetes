c265a96a28f286fe9f7100e3e6c39bdf5e3569b4
svc.wf-jenkins <svc.wf-jenkins@vmware.com>
2022-09-12 11:30:48 -0700

    add CRD

--- internal/util/util.go
+++
@@ -1,9 +0,0 @@
-package util
-
-const Namespace = "wavefront"
-const ProxyName = "wavefront-proxy"
-const ClusterCollectorName = "wavefront-cluster-collector"
-const NodeCollectorName = "wavefront-node-collector"
-const LoggingName = "wavefront-logging"
-const Deployment = "Deployment"
-const DaemonSet = "DaemonSet"
