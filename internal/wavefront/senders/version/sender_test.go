544213dd1dc35ca03b6f6148149c150ed4da56cc
Matthew Conger-Eldeen <matthewco@vmware.com>
2022-09-30 15:54:10 -0600

    use the existing testhelper.MockMetricClient to test version sender
    Signed-off-by: Yuqi Jin <jyuqi@vmware.com>

--- internal/wavefront/senders/version/sender_test.go
+++ internal/wavefront/senders/version/sender_test.go
@@ -1,47 +1,19 @@
 package version_test
 
 import (
-	"github.com/stretchr/testify/require"
+	"github.com/wavefrontHQ/wavefront-operator-for-kubernetes/internal/testhelper"
 	"github.com/wavefrontHQ/wavefront-operator-for-kubernetes/internal/wavefront/senders/version"
 	"testing"
 )
 
 func TestSender(t *testing.T) {
 	t.Run("sends simple semantic versions to wavefront", func(t *testing.T) {
-		expectVersionClient := NewExpectVersionClient(2.013, "somecluster")
-		version.NewSender(expectVersionClient).SendVersion("2.1.3", "somecluster")
+		expectedMetricLine := testhelper.NewMockMetricClient(testhelper.AssertContainsLine(
+			"kubernetes.observability.version 2.013 source=\"somecluster\"",
+		))
 
-		expectVersionClient.Verify(t)
+		_ = version.NewSender(expectedMetricLine).SendVersion("2.1.3", "somecluster")
+
+		expectedMetricLine.Verify(t)
 	})
-}
-
-type ExpectedVersionClient struct {
-	expectedVersion float64
-	actualVersion   float64
-	expectedCluster string
-	actualCluster   string
-}
-
-func NewExpectVersionClient(expectedVersion float64, expectedCluster string) *ExpectedVersionClient {
-	return &ExpectedVersionClient{
-		expectedVersion: expectedVersion,
-		expectedCluster: expectedCluster,
-	}
-}
-
-func (e *ExpectedVersionClient) SendMetric(name string, value float64, ts int64, source string, tags map[string]string) error {
-	e.actualVersion = value
-	e.actualCluster = tags["cluster"]
-	return nil
-}
-
-func (e *ExpectedVersionClient) Flush() error {
-	return nil
-}
-
-func (e *ExpectedVersionClient) Close() {}
-
-func (e *ExpectedVersionClient) Verify(t *testing.T) {
-	require.Equal(t, e.expectedVersion, e.actualVersion)
-	require.Equal(t, e.expectedCluster, e.actualCluster)
 }
