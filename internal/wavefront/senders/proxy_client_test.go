e34c8c455f9247300328e103838e06841d27d380
Matthew Conger-Eldeen <matthewco@vmware.com>
2022-10-03 14:36:08 -0600

    BORKWIP: remove unnecessary sender object and convert to function
    Co-authored-by: John Cornish <jcornish@vmware.com>

---
+++ internal/wavefront/senders/proxy_client_test.go
@@ -0,0 +1,15 @@
+package senders_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/wavefrontHQ/wavefront-operator-for-kubernetes/internal/wavefront/senders"
+	"testing"
+)
+
+func TestWavefrontProxySender(t *testing.T) {
+	t.Run("passes on wfsdk.Sender creation errors", func(t *testing.T) {
+		_, err := senders.NewWavefrontProxySender("")
+
+		assert.Error(t, err)
+	})
+}
