0ef75dcaf13d91ac23423317728606946eaa8368
Yuqi Jin <jyuqi@vmware.com>
2022-10-24 15:19:14 -0600

    upgrade docker buildx
    Signed-off-by: Matthew Conger-Eldeen <matthewco@vmware.com>

--- hack/jenkins/install_docker_buildx.sh
+++ hack/jenkins/install_docker_buildx.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
+rm "$HOME/.docker/cli-plugins/docker-buildx"
 if [[ ! -f "$HOME/.docker/cli-plugins/docker-buildx" ]]; then
   echo "installing docker buildx..."
-  wget -q -O docker-buildx https://github.com/docker/buildx/releases/download/v0.6.3/buildx-v0.6.3.linux-amd64
+  wget -q -O docker-buildx https://github.com/docker/buildx/releases/download/v0.9.1/buildx-v0.9.1.linux-amd64
   chmod a+x docker-buildx
   [[ ! -d "$HOME/.docker/cli-plugins" ]] && mkdir -p ~/.docker/cli-plugins
   mv docker-buildx ~/.docker/cli-plugins
