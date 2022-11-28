138e3d5e6b317f8cab4b346ab7678679ec5c77aa
John Cornish <jcornish@vmware.com>
2022-11-28 12:08:57 -0700

    for freakin real?
    Co-authored-by: Matthew Conger-Eldeen <matthewco@vmware.com>
    Co-authored-by: Mark Michael <mamichael@vmware.com>

--- hack/concourse/tasks/generate-pipeline.sh
+++ hack/concourse/tasks/generate-pipeline.sh
@@ -8,7 +8,7 @@ function get_resources() {
   echo "resources:"
   echo "#@overlay/append"
   for feature_branch in "${@}" ; do
-    jira=$(echo $feature_branch | grep -o 'K8SSAAS-[0-9]+')
+    jira=$(echo $feature_branch | grep -o 'K8SSAAS-[0-9]*')
     cat <<- EOD
 - name: wavefront-operator-${jira}
   type: git
