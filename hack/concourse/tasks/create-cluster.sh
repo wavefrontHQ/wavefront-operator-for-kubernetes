9bdfd2f11d149e22409bb7c799a8446bf59f02dc
John Cornish <jcornish@vmware.com>
2022-11-28 15:00:28 -0700

    Fake create cluster to see if it works
    Co-authored-by: Matthew Conger-Eldeen <matthewco@vmware.com>
    Co-authored-by: Mark Michael <mamichael@vmware.com>

---
+++ hack/concourse/tasks/create-cluster.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo GKE_CLUSTER="$CLUSTER_NAME" make gke-create-cluster
--- hack/concourse/tasks/generate-pipeline.sh
+++ hack/concourse/tasks/generate-pipeline.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+function ensure_ytt() {
+  if ! command -v wget; then
+    apt update
+    apt install -y wget
+  fi
+
+  if ! command -v ytt; then
+    wget https://github.com/vmware-tanzu/carvel-ytt/releases/download/v0.43.0/ytt-linux-amd64
+    chmod +x ytt-linux-amd64
+    mv ytt-linux-amd64 /usr/local/bin/ytt
+    ytt version
+  fi
+}
+
 function get_resources() {
   cat << EOF
 #@data/values
@@ -21,26 +35,41 @@ EOD
   done
 }
 
-function ensure_ytt() {
-  if ! command -v wget; then
-    apt update
-    apt install -y wget
-  fi
-
-  if ! command -v ytt; then
-    wget https://github.com/vmware-tanzu/carvel-ytt/releases/download/v0.43.0/ytt-linux-amd64
-    chmod +x ytt-linux-amd64
-    mv ytt-linux-amd64 /usr/local/bin/ytt
-    ytt version
-  fi
+function get_jobs() {
+  cat << EOF
+#@data/values
+#@ load("@ytt:overlay", "overlay")
+---
+jobs:
+#@overlay/append
+EOF
+  for feature_branch in "${@}" ; do
+    jira=$(echo $feature_branch | grep -o 'K8SSAAS-[0-9]*')
+    cat <<- EOD
+- name: setup-cluster-${jira}
+  plan:
+    - get: wavefront-operator-ci
+      passed: [update-self]
+    - get: wavefront-operator-${jira}
+      trigger: true
+    - task: create-cluster-${jira}
+      file: wavefront-operator-ci/hack/concourse/tasks/create-cluster.yaml
+      input_mapping:
+        ci_repo: wavefront-operator-ci
+      params:
+        CLUSTER_NAME: k8po-feature-cluster-${jira}
+EOD
+  done
 }
 
 ensure_ytt
 
 echo "Generating pipeline"
 get_resources $(cat feature_branches/list.txt) > ci_repo/hack/concourse/yamlbits/feature_branch_resources.yaml
+get_jobs $(cat feature_branches/list.txt) > ci_repo/hack/concourse/yamlbits/feature_branch_jobs.yaml
 
 ytt -f ci_repo/hack/concourse/yamlbits \
-  > pipeline.yaml
+  > generated-pipeline/pipeline.yaml
 
-cat pipeline.yaml
+echo "Generated Pipeline:"
+cat generated-pipeline/pipeline.yaml
