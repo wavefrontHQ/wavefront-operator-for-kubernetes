a2b65de9542f51a13c884a55397a33831cd89143
John Cornish <jcornish@vmware.com>
2023-01-11 13:50:47 -0700

    WIP: tmp file for run.sh

--- hack/test/validation/run.sh
+++ hack/test/validation/run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-go run hack/test/validation/main.go <(yq .spec.versions.0.schema.openAPIV3Schema config/crd/bases/wavefront.com_wavefronts.yaml)
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+yq .spec.versions.0.schema.openAPIV3Schema ${REPO_ROOT}/config/crd/bases/wavefront.com_wavefronts.yaml > com_wavefront_schema_extract.yaml
+
+go run ${REPO_ROOT}/hack/test/validation/main.go com_wavefront_schema_extract.yaml
